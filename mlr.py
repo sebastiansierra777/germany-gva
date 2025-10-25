1153ad3bc229f94a51e65fb7adb32433c3477555
sebastiansierra777 <sebastiansierraocm@gmail.com>
2025-10-25 13:52:49 -0500

    Initial commit: Setup project with gitignore and first file

---
+++ mlr.py
@@ -0,0 +1,13 @@
+# %%
+import pandas as pd
+print("Hello World")
+
+# %%
+data = {'country': ['USA', 'UK','Germany'],
+        'capital': ['Washington DC', 'London', 'Berlin']}
+df = pd.DataFrame(data)
+print(df)
+
+# %%
+print("Each cell can run independently!")
+
